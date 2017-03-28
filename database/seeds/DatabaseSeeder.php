544b58ae96f4ea88565e6df906ec865e5be80f2c
vagrant <vagrant@homestead>
2017-03-28 05:21:39 +0000

    started seeding spots

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	Eloquent::unguard();
+
+    	$this->call('MySpotSeeder');
+    	$this->command->info('MySpot seeds finished');
     }
+
+
+}
+
+// custom seeder class 
+// TODO: move to file
+
+class MySpotSeeder extends Seeder {
+
+	public function run() {
+
+		// clear database
+		DB::table('spots')->delete();
+		DB::table('lists')->delete();
+		DB::table('users')->delete();
+		DB::table('tags')->delete();
+
+		DB::table('list_user')->delete();
+		DB::table('list_spot')->delete();
+
+		// seed spots table
+
+		$spotPuccini = Spot::create(array(
+			'name'		=> 'Puccini',
+			'address'	=> 'Dogpatch',
+			'notes'		=> 'One day we gotta go here',
+		));
+
+		$spotChezMaman = Spot::create(array(
+			'name'		=> 'Chez Maman',
+			'address'	=> 'Potrero',
+			'notes'		=> 'SALAD'))
+		));
+	}
 }
